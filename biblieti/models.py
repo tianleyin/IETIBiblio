c10092760c99d9bea207be65d2a4fdd7105f2405
IFurio <ifuriomartin.cf@iesesteveterradas.cat>
2024-04-19 18:27:39 +0200

    implemented admin

--- biblieti/models.py
+++ biblieti/models.py
@@ -21,6 +21,8 @@ class Catalogue(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     picture = models.ImageField(upload_to='imgs/', blank=True, null=True)
+    def __str__(self):
+        return self.name
 
 class Book(Catalogue):
     author = models.CharField(max_length=100)
@@ -47,7 +49,7 @@ class Booking(models.Model):
     id = models.AutoField(primary_key=True)
     catalogue = models.ForeignKey(Catalogue, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    booking_date = models.DateTimeField()
+    booking_date = models.DateTimeField(auto_now_add=True)
 
 class Loan(models.Model):
     id = models.AutoField(primary_key=True)
