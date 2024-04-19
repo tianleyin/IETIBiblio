83d9fffc879b857a580d784d092c90e376686cc0
ErikTamano <72156568+ErikTamanoSantos@users.noreply.github.com>
2024-04-19 19:32:27 +0200

    advanced filter in progress

--- biblieti/static/search_product_controller.js
+++ biblieti/static/search_product_controller.js
@@ -1,6 +1,31 @@
 $(() => {
     $("#expand-header").off().on("click", () => {
-        console.log("A")
         $(".expanding-header").toggleClass("expanded")
+    })
+
+    $("#product-type").off().on("change", function () {
+        let type = $("#product-type").val()
+        switch (type) {
+            case "Book":
+                $("#dynamic-fieldset").html(`
+                <fieldset>
+                    <legend>Filtres avançats</legend>
+                    <div class="form-row space-around">
+                        <div>
+                            <label for="author">Autor</label>
+                            <input type="text" id="author" name="author" placeholder="Autor">
+                        </div>
+                        <div>
+                            <label for="ISBN">ISBN</label>
+                            <input type="text" id="ISBN" name="ISBN" placeholder="ISBN">
+                        </div>
+                        <div>
+                            <label for="publish-year">Any de publicació</label>
+                            <input type="number" id="publish-year" name="publish-year" placeholder="Any de publicació">
+                        </div>
+                    </div>
+                </fieldset>
+                `)
+        }
     })
 })
