9a899256770ed71eabe6470f387855e156ed7ca4
Isaac <ifuriomartin.cf@iesesteveterradas.cat>
2024-04-20 21:08:47 +0200

    implemented basic structure to test in proxmox

--- biblieti/views.py
+++ biblieti/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def response(request):
+    return HttpResponse("<h1>Django funcionando</h1>")
