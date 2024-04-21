c58691bc7ea1cc43a1c6dd7dd24c0459bb1cb668
ErikTamano <eriktamano@gmail.com>
2024-04-21 18:56:55 +0200

    Advanced search header done

--- biblieti/static/search_product_controller.js
+++ biblieti/static/search_product_controller.js
@@ -6,6 +6,9 @@ $(() => {
     $("#product-type").off().on("change", function () {
         let type = $("#product-type").val()
         switch (type) {
+            case "Any":
+                $("#dynamic-fieldset").html("")
+                break
             case "Book":
                 $("#dynamic-fieldset").html(`
                 <fieldset>
@@ -26,6 +29,81 @@ $(() => {
                     </div>
                 </fieldset>
                 `)
+                break
+            case "CD":
+                $("#dynamic-fieldset").html(`
+                <fieldset>
+                    <legend>Filtres avançats</legend>
+                    <div class="form-row space-around">
+                        <div>
+                            <label for="artist">Artista</label>
+                            <input type="text" id="artist" name="artist" placeholder="Artista">
+                        </div>
+                        <div>
+                            <label for="tracks">Nombre de pistes</label>
+                            <input type="number" id="tracks" name="tracks" placeholder="Nombre de pistes">
+                        </div>
+                        <div>
+                        </div>
+                    </div>
+                </fieldset>
+                `)
+                break
+            case "DVD":
+                $("#dynamic-fieldset").html(`
+                <fieldset>
+                    <legend>Filtres avançats</legend>
+                    <div class="form-row space-around">
+                        <div>
+                            <label for="director">Director</label>
+                            <input type="text" id="director" name="director" placeholder="Director">
+                        </div>
+                        <div>
+                            <label for="duration">Duració</label>
+                            <input type="time" id="duration" name="duration" placeholder="Duració">
+                        </div>
+                        <div>
+                        </div>
+                    </div>
+                </fieldset>
+                `)
+                break
+            case "BR":
+                $("#dynamic-fieldset").html(`
+                <fieldset>
+                    <legend>Filtres avançats</legend>
+                    <div class="form-row space-around">
+                        <div>
+                            <label for="resolution">Resolució</label>
+                            <input type="text" id="resolution" name="resolution" placeholder="Resolution">
+                        </div>
+                        <div>
+                        </div>
+                        <div>
+                        </div>
+                    </div>
+                </fieldset>
+                `)
+                break
+            case "Device":
+                $("#dynamic-fieldset").html(`
+                <fieldset>
+                    <legend>Filtres avançats</legend>
+                    <div class="form-row space-around">
+                        <div>
+                            <label for="manufacturer">Manufacturador</label>
+                            <input type="text" id="manufacturer" name="manufacturer" placeholder="Manufacturador">
+                        </div>
+                        <div>
+                            <label for="model">Model</label>
+                            <input type="text" id="model" name="model" placeholder="Model">
+                        </div>
+                        <div>
+                        </div>
+                    </div>
+                </fieldset>
+                `)
+                break
         }
     })
 })
