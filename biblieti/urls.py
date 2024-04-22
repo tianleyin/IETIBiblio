281f2b5101a920eefc4f7ce6a9e58c7b59b268a6
Tianle <tianleyin8888@gmail.com>
2024-04-22 16:32:28 +0200

    PreMerge

--- biblieti/urls.py
+++ biblieti/urls.py
@@ -1,8 +1,10 @@
 # urls.py
 from django.urls import path
 from . import views
+from .views import *
 
 urlpatterns = [
     path('', views.landing_page, name='landing_page'),
+    path('login/', user_login, name='login'),
     # Otras URLs aquí
 ]
--- biblieti/views.py
+++ biblieti/views.py
@@ -1,5 +1,21 @@
 # views.py
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 
 def landing_page(request):
-    return render(request, 'landing_page.html')
+    return render(request, 'biblieti/landing_page.html')
+
+
+def user_login(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        User = authenticate(request, email=email, password=password)
+        if User is not None:
+            login(request, User)
+            return redirect('dashboard')  # Redirigir a la página de inicio después del inicio de sesión exitoso
+        else:
+            # Mostrar un mensaje de error de inicio de sesión en caso de credenciales incorrectas
+            return render(request, 'biblieti/landing_page.html', {'error': 'Credenciales inválidas'})
+    else:
+        return render(request, 'biblieti/landing_page.html')
