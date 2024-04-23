bd3f4e3ee3de0afdb17fbcbcb260c73b70277660
IFurio <ifuriomartin.cf@iesesteveterradas.cat>
2024-04-23 17:05:49 +0200

    fixed create_user script

--- biblieti/management/commands/create_user.py
+++ biblieti/management/commands/create_user.py
@@ -1,8 +1,11 @@
 from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand, CommandError
 
-# Obtén el modelo de usuario personalizado
-User = get_user_model()
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        # Obtén el modelo de usuario personalizado
+        User = get_user_model()
 
-# Crea un usuario utilizando el modelo personalizado
-user = User.objects.create_user("isaac", "ifuriomartin.cf@iesesteveterradas.cat", "isaac123")
-user.save()
+        # Crea un usuario utilizando el modelo personalizado
+        user = User.objects.create_user("isaac", "ifuriomartin.cf@iesesteveterradas.cat", "isaac123")
+        user.save()
