841e1e065558278b67c5cf7b219b85c2eba8d6e2
IFurio <ifuriomartin.cf@iesesteveterradas.cat>
2024-04-23 17:00:30 +0200

    fixed create_user script

--- biblieti/management/commands/create_user.py
+++ biblieti/management/commands/create_user.py
@@ -1,4 +1,8 @@
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
+# Obtén el modelo de usuario personalizado
+User = get_user_model()
+
+# Crea un usuario utilizando el modelo personalizado
 user = User.objects.create_user("isaac", "ifuriomartin.cf@iesesteveterradas.cat", "isaac123")
 user.save()
