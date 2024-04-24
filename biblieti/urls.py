6a660765d20cd4f57cde9f0369da789873f77115
Isaac <ifuriomartin.cf@iesesteveterradas.cat>
2024-04-24 05:08:05 +0200

    test of reverse_lazy redirect in change_password

--- biblieti/urls.py
+++ biblieti/urls.py
@@ -30,6 +30,6 @@ urlpatterns = [
     path('', login_view, name='login'),
     path('forgot_password/', auth_views.PasswordResetView.as_view(), name='forgot_password'),
     path('logout/', auth_views.LogoutView.as_view(template_name='logout_custom.html'), name='logout'),
-    path('change_password/', auth_views.PasswordChangeView.as_view(), name='change_password'),
+    path('change_password/', auth_views.PasswordChangeView.as_view(success_url=reverse_lazy('dashboard')), name='change_password'),
     path('user_data/', user_data, name='user_data'),
 ]
